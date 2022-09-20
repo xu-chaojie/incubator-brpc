cfd00a6f08b29d5328953e2ee9b2b28df7d14b50
Xu Yifeng <yfxu@NetEase.inc>
2022-09-20 16:27:07 +0800

    Restore code, reduce diff.

--- src/brpc/input_messenger.cpp
+++ src/brpc/input_messenger.cpp
@@ -422,8 +422,10 @@ int InputMessenger::Create(const butil::EndPoint& remote_side,
                            SocketId* id) {
     SocketOptions options;
     options.remote_side = remote_side;
+    options.user = this;
+    options.on_edge_triggered_events = OnNewMessages;
     options.health_check_interval_s = health_check_interval_s;
-    return this->Create(options, id);
+    return Socket::Create(options, id);
 }
 
 int InputMessenger::Create(SocketOptions options, SocketId* id) {
